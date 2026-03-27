f276f12050851cdaa84282ca4fbae1667b1ec38c
Darshan Gowda <96766940+darshansrc@users.noreply.github.com>
2026-03-27 20:42:38 +0530

    [feat]: setup project & theme

---
+++ components/shared/footer.tsx
@@ -0,0 +1,27 @@
+import { Button } from "../ui/button";
+
+export function Footer() {
+  return (
+    <section className="flex flex-col gap-6 xl:gap-8 items-center justify-center text-center bg-asymmetri-red min-h-screen w-full">
+      <h2 className="font-chakra-petch font-medium text-2xl lg:text-4xl xl:text-5xl">
+        Let’s turn your ideas into <br /> beautiful asymmetry.
+      </h2>
+
+      <div className="flex flex-row gap-2 xl:gap-4">
+        <Button
+          size="lg"
+          className="font-chakra-petch text-asymmetri-red py-4 font-semibold"
+        >
+          Get in touch
+        </Button>
+        <Button
+          size="lg"
+          className="font-chakra-petch bg-transparent font-semibold"
+          variant={"outline"}
+        >
+          Join our team
+        </Button>
+      </div>
+    </section>
+  );
+}
